0451b54d9dd3d5ad8e30e73014b0a1145c78ceda
del3500 <wendellsmesia@gmail.com>
2024-11-20 14:18:07 +0800

    finished URLPaths

--- DNS/URI/URLPaths/main.go
+++ DNS/URI/URLPaths/main.go
@@ -7,34 +7,34 @@ import (
 	"net/http"
 )
 
-const url = "https://api.boot.dev/v1/courses_rest_api/learn-http/"
+const url = "https://api.boot.dev/v1/courses_rest_api/learn-http/projects"
+
+type Projects struct {
+	Id        string `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+	Assignees int    `json:"assignees"`
+}
 
 func main() {
-
-	res, err := http.Get(url + "locations")
+	res, err := http.Get(url)
 	if err != nil {
-		log.Fatalf("error creating rƒequest: %v", err)
+		log.Fatalf("error creating request: %v", err)
 	}
 	defer res.Body.Close()
 
-	var locations []Location
+	var projects []Projects
 	decoder := json.NewDecoder(res.Body)
-	err = decoder.Decode(&locations)
+	err = decoder.Decode(&projects)
 	if err != nil {
 		log.Fatalf("error encoding response: %v", err)
 	}
-	logLocations(locations)
-}
 
-type Location struct {
-	Discovered       bool   `json:"discovered"`
-	Id               string `json:"id"`
-	Name             string `json:"name"`
-	RecommendedLevel int    `json:"recommendedLevel"`
-}
-
-func logLocations(locations []Location) {
-	for _, l := range locations {
-		fmt.Printf("Location: %s\nLevel:%v\n", l.Name, l.RecommendedLevel)
+	for _, p := range projects {
+		fmt.Printf("Title: %s\n", p.Title)
+		fmt.Printf("ID: %s\n", p.Id)
+		fmt.Printf("Completed: %t\n", p.Completed)
+		fmt.Printf("Assignees: %d\n", p.Assignees)
+		fmt.Println("=======================")
 	}
 }
